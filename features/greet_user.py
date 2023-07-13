aa48ce9f0b0269dc6cad40793b64f263366a8435
OJHacker <ojoshi387@gmail.com>
2023-07-13 12:07:57 +0530

    Added audio and greet user functions

---
+++ features/greet_user.py
@@ -0,0 +1,17 @@
+from features.audio import say
+import datetime
+
+def greetuser():
+    hour = int(datetime.datetime.now().hour)
+    if hour >=0 and hour < 12:
+        say("Good morning")
+        print("Good morning")
+    elif hour >= 12 and hour < 17:
+        say("Good afternoon")
+        print("Good afternoon")
+    elif hour >= 17 and hour < 20:
+        say("Good evening")
+        print("Good evening")
+    else:
+        say("Good night")
+        print("Good night")
