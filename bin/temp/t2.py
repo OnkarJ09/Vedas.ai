d2f18a32a52c3819c194fc92d1abfb6a9a3ddff6
OJHacker <ojoshi387@gmail.com>
2024-04-28 16:30:18 +0530

    fasttext working in progress

--- bin/temp/t2.py
+++ bin/temp/t2.py
@@ -8,6 +8,18 @@ from gtts import gTTS
 import os
 import playsound
 import langid
+from textblob import TextBlob
+from langdetect import detect
+import fasttext
+
+
+model = fasttext.load_model('lid.176.bin')
+lang = model.predict()
+
+def detect_lang(text):
+    model = fasttext.load_model('lid.176.bin')
+    lang = model.predict(text, k=1)[0]
+    return lang[0].replace("__label__", "")
 
 
 def translate_text(text, target_language):
@@ -26,14 +38,15 @@ def take_command():
         audio = r.listen(source)
     try:
         print("Recognizing...")
-
+        # lang_code = detect_lang(audio)
         #todo: Add here the langid para/ function to detect the language
 
-        query = r.recognize_google(audio, language='hi')
+        query = r.recognize_google(audio, language=lang_code)
         # target_language = "en"
         # query = translator.translate_text(text_to_translate, target_language)
         print(f"User said: {query}\n")
         return query
+
     except Exception as e:
         print(e)
         print("Say that again please...")
@@ -41,16 +54,18 @@ def take_command():
     return 'None'
 
 
-def say(audio, lang='hi'):
+def say(audio, lang=lang_code):
     tts = gTTS(text=audio, lang=lang, slow=False)
     tts.save("output.mp3")
     playsound.playsound("output.mp3")
 
 
-translations = translate_text(take_command(), "en")
-again_trans = translate_text(translations, "hi")
-say(again_trans)
-print(again_trans)
+# translations = translate_text(take_command(), "en")
+# again_trans = translate_text(translations, "hi")
+# say(again_trans)
+# print(again_trans)
+print(detect_lang("olá, como vai você irmão, ram ram"))
+say(detect_lang("olá, como vai você irmão, ram ram"))
 
 # say("hello")
 # Example usage
---
+++ bin/temp/t3.py
@@ -0,0 +1,7 @@
+import speech_recognition as sr
+import requests
+from gtts import gTTS
+import playsound
+import fasttext
+
+
