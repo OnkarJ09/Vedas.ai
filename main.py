47e45697661fd3565365a336fa83d1fb4571873b
Mad_Dog_OJ <ojhacker.master@gmail.com>
2025-12-15 17:14:14 +0530

    Implemented the plugin manager logic to run different skills.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from skills import *
-import logging, dotenv
+from skills.greet_user_acc_time import get_time_based_greeting
+from manager.plugin_manager import PluginManager
+from dotenv import load_dotenv
+import logging, os
+
+
+# ----------------  INITIALIZE DOTENV   -------------- #
+load_dotenv()
 
 # ---------------- LOGGER SETUP ---------------- #
 logger = logging.getLogger("VEDAS")
@@ -14,13 +20,38 @@ logger.addHandler(file_handler)
 
 # ---------------- VEDAS CORE ---------------- #
 if __name__ == "__main__":
+    # initialize plugin manager
+    plugin_manager = PluginManager()
+
+    # define the plugin dir.
+    plugin_manager.add_directory("skills")
+
+    # load all the skills/plugins in dir with "Vedas" class
+    plugin_manager.load_plugins()
+
+    # greet user
     get_time_based_greeting()
+    logger.info(f"Greeting with new session {get_time_based_greeting()}")
 
     while True:
         query = input("User -> ").lower()
+        logger.info(f"User -> {query}")     # user query log..
+
+        if query is None:
+            continue
 
         if query == "exit":
+            logger.info("Goodbye!!....")
             break
+
+        elif query == "help":
+            #TODO: add a help skill and implement it here or with plugin it self
+            pass
+
+        else:
+            plugin_manager.execute_plugin(query)
+            logger.info(f"VEDAS -> ")
+
 
 
 
