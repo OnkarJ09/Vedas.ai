3488fc8ba15beff2ba60efa3bbd24884b76818d2
mad_dog <ojoshi387@gmail.com>
2026-05-06 03:36:44 +0530

    decided to skip the mcp-use as it was not giving the expected output

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-# server.py
-from mcp.server import FastMCP
+"""
+Removing the mcp-use...
 
-server = FastMCP("demo-server")
-
-@server.tool()
-async def add_numbers(a: float, b: float) -> str:
-    return f"{a + b}"
-
-@server.resource("config://app")
-async def get_app_config() -> str:
-    return "App: demo, Version: 0.1.0, Status: Active"
-
-@server.prompt()
-async def assistant_prompt() -> str:
-    return "You are a helpful assistant for demo. Be concise and friendly."
-
-if __name__ == "__main__":
-    server.run("stdio")
+Now using a custom framework and a custom manager to integrate the same behavior as the mcp-use!!!
+"""
